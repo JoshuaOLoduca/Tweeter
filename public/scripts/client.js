5e6a096081ab4457b0193f3b995c144fbc679ce4
JoshuaOLoduca <81768747+JoshuaOLoduca@users.noreply.github.com>
2022-01-31 19:47:40 -0500

    fixed name/img alignment on tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,22 +4,35 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const template = [{
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
+const template = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png",
       "handle": "@SirIsaac"
     },
-  "content": {
+    "content": {
       "text": "If I have seen further it is by standing on the shoulders of giants"
     },
-  "created_at": 1461116232227
-}]
+    "created_at": 1643494518506
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd"
+    },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1643580918506
+  }
+]
 
 const createTweetElement = tweetInfo => {
   const tweet = $(`<article class="tweet">
     <header>
-      <span><img src="${tweetInfo.user.avatars}" />${tweetInfo.user.name}</span>
+      <span><img src="${tweetInfo.user.avatars}">${tweetInfo.user.name}</span>
       <span class="handle">${tweetInfo.user.handle}</span>
     </header>
     <p>${tweetInfo.content.text}</p>
